6e42f460210bd74bc78f7d48a6c2e44ba84d55ba
Yukohana <jpsky@outlook.jp>
2025-03-25 11:40:01 +0900

    kadai021

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('btn');
+  const text = document.getElementById('text');
+
+
+  btn.addEventListener('click', () => {
+    setTimeout(() => {
+      text.textContent = "ボタンをクリックしました";
+    }, 2000);
+  });
+});
